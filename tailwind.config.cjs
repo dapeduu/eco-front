b958b34f64c2d7c8d428ff30ce5d412ce1d8dd29
Pedro Santos <pedrosantosdevelop@gmail.com>
2022-08-15 11:53:14 +0000

    adding color and button component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,12 @@ module.exports = {
   content: ["./index.html",
     "./src/**/*.tsx",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#92E3A9",
+        'header-bg-color': "#6EAB7F"
+      },
+    },
   },
   plugins: [],
 }
