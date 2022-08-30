f718cdc39c6e6967444732ed7fae1a5a85fbd3c5
Pedro Santos <pedrosantosdevelop@gmail.com>
2022-08-30 22:18:55 +0000

    adds menu page styles

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom"
+
 export const Menu = () => {
   return (
-    <div>Menu</div>
+    <div className="full-page-container">
+      <main className="flex flex-col w-full gap-5 max-w-lg px-4">
+        <Link to="/map" className="btn-primary">Ver pontos de coleta</Link>
+        <Link to="/qr_code_reader" className="btn-secondary">Ler QR Code</Link>
+        <Link to="/delivery_list" className="btn-secondary">Ver entregas passadas</Link>
+        <Link to="/redeem_coupons" className="btn-secondary">Resgatar E-CO points</Link>
+        <Link to="/" className="btn-secondary">SAIR</Link>
+      </main>
+    </div>
   )
 }
