8de531da6e2694d7587269e66eb675ca574d0ce4
Pedro Santos <pedrosantosdevelop@gmail.com>
2022-08-30 23:44:05 +0000

    adds center component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -11,19 +11,21 @@ export const Login = () => {
 
   return (
     <div className="full-page-container">
-      <main className="h-96 ">
-        <form className="h-full flex flex-col gap-4 justify-between" onSubmit={handleLogin}>
-          <div className="text-center px-">
-            <img className='mx-auto' src={LoginImage} alt="Illustration of person holding a plant" />
-            <h1 className="text-2xl font-light">E-CO</h1>
-          </div>
-          <div className="flex flex-col gap-4">
-            <input className="input" id="cpf" required type="text" placeholder='Insira seu CPF' />
-            <input className="input" id="password" required type="password" placeholder='Insira sua senha' />
-          </div>
-          <button className="btn-primary" type="submit">Entrar</button>
-        </form>
-      </main>
+      <div className="center">
+        <main className="h-96 ">
+          <form className="h-full flex flex-col gap-4 justify-between" onSubmit={handleLogin}>
+            <div className="text-center px-">
+              <img className='mx-auto' src={LoginImage} alt="Illustration of person holding a plant" />
+              <h1 className="text-2xl font-light">E-CO</h1>
+            </div>
+            <div className="flex flex-col gap-4">
+              <input className="input" id="cpf" required type="text" placeholder='Insira seu CPF' />
+              <input className="input" id="password" required type="password" placeholder='Insira sua senha' />
+            </div>
+            <button className="btn-primary" type="submit">Entrar</button>
+          </form>
+        </main>
+      </div>
     </div>
   )
 }
