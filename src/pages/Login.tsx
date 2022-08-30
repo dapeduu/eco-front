8a08db0a031b73e06b045de7ddf39000af032ba3
Pedro Santos <pedrosantosdevelop@gmail.com>
2022-08-30 21:49:10 +0000

    adds the login page initial styles

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,21 @@
+import LoginImage from '../assets/login.svg'
+
 export const Login = () => {
   return (
-    <div>Login</div>
+    <div className="w-screen h-screen flex items-center justify-center">
+      <main className="h-96 ">
+        <form className="h-full flex flex-col gap-4 justify-between">
+          <div className="text-center px-">
+            <img className='mx-auto' src={LoginImage} alt="Illustration of person holding a plant" />
+            <h1 className="text-2xl font-light">E-CO</h1>
+          </div>
+          <div className="flex flex-col gap-4">
+            <input className="input" id="cpf" required type="text" placeholder='Insira seu CPF' />
+            <input className="input" id="password" required type="password" placeholder='Insira sua senha' />
+          </div>
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+      </main>
+    </div>
   )
 }
