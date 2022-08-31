30dc146d99329b1adf538cf134aa4bc211ca078a
Pedro Santos <pedrosantosdevelop@gmail.com>
2022-08-31 21:35:10 +0000

    adds qrcode page

--- src/pages/QrCodeReader.tsx
+++ src/pages/QrCodeReader.tsx
@@ -1,5 +1,27 @@
+
+import { OnResultFunction, QrReader } from 'react-qr-reader';
+import { useNavigate } from 'react-router-dom';
+
 export const QrCodeReader = () => {
+  const navigate = useNavigate();
+
+  const onResult: OnResultFunction = (result, error) => {
+    if (error) return console.info("Error while reading qrcode")
+
+    console.log(result)
+  }
+
   return (
-    <div>QrCodeReader</div>
+    <div className='flex flex-col h-screen max-h-screen'>
+      <div className='h-36 w-full bg-black text-white backdrop-blur-md px-2 py-4'>
+        <div className='container mx-auto max-w-lg'>
+          <button className="text-2xl" onClick={() => navigate(-1)}>←</button>
+          <h1 className='text-2xl text-center'>Mire no código QR gerado pelo ponto de coleta</h1>
+        </div>
+      </div>
+      <main className='flex items-center justify-center'>
+        <QrReader constraints={{}} className="w-full my-auto" onResult={onResult} />
+      </main>
+    </div>
   )
 }
