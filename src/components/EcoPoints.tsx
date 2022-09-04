aa66c1ca224246bec63e8b447fe9e429f462136f
Pedro Santos <pedrosantosdevelop@gmail.com>
2022-09-04 13:36:48 +0000

    updates eco points component

--- src/components/EcoPoints.tsx
+++ src/components/EcoPoints.tsx
@@ -1,12 +1,27 @@
 import EcoLogo from '../assets/eco-points.svg'
 
-export const EcoPoints = () => {
-  const ecoPoints = 3000 // TODO: Get it from the global state
+type EcoPointsProps = {
+  value?: number
+}
+
+/**
+ * If a value is provided, the component will render with the provided value.
+ * If not, it will show the user E-CO points.
+ */
+export const EcoPoints = ({ value }: EcoPointsProps) => {
+  if (value) return (
+    <span>
+      <img className="inline-block mr-1" src={EcoLogo} alt="Símbolo dos Eco points. É ícone de folha de árvore." />
+      {value}
+    </span>
+  )
+
+  const userEcoPoints = 3000 // TODO: get it from the global state
 
   return (
     <span>
       <img className="inline-block mr-1" src={EcoLogo} alt="Símbolo dos Eco points. É ícone de folha de árvore." />
-      {ecoPoints}
+      {userEcoPoints}
     </span>
   )
 }
