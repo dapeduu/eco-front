b3f3f1cb94e322cbe8481fe9054d235d40607ce6
Pedro Santos <pedrosantosdevelop@gmail.com>
2022-09-04 14:19:57 +0000

    fixes delivery list spacing

--- src/pages/DeliveryList.tsx
+++ src/pages/DeliveryList.tsx
@@ -6,7 +6,10 @@ export const DeliveryList = () => {
     <NavbarLayout withBackButton>
       <main className="container mx-auto max-w-lg p-3 ">
         <h1 className="text-2xl text-center my-3">Minhas entregas</h1>
-        <ul className="list-none ">
+        <ul className="list-none flex flex-col gap-4">
+          <li>
+            <DeliveryItem name="Bateria" situation="Em transito" id="123412" />
+          </li>
           <li>
             <DeliveryItem name="Bateria" situation="Em transito" id="123412" />
           </li>
