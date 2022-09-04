e95353c7d5dbe6a4f6b5ef6730c7989a195ac402
Pedro Santos <pedrosantosdevelop@gmail.com>
2022-09-04 22:51:03 +0000

    fixes qr code styles

--- src/pages/QrCodeReader.tsx
+++ src/pages/QrCodeReader.tsx
@@ -1,10 +1,7 @@
-
-import { useState } from 'react';
 import { OnResultFunction, QrReader } from 'react-qr-reader';
 import { useNavigate } from 'react-router-dom';
 
 export const QrCodeReader = () => {
-  const [shouldRenderQrReader, setShouldRenderQrReader] = useState(true)
   const navigate = useNavigate();
 
   const onResult: OnResultFunction = (result, error) => {
@@ -14,7 +11,7 @@ export const QrCodeReader = () => {
 
     if (isCodeValid) {
       navigate('/delivery_confirmation')
-      setShouldRenderQrReader(false)
+      console.log('Scanner Result: ' + result?.getText())
     }
   }
 
@@ -26,8 +23,8 @@ export const QrCodeReader = () => {
           <h1 className='text-2xl text-center'>Mire no código QR gerado pelo ponto de coleta</h1>
         </div>
       </div>
-      <main className='flex items-center justify-center'>
-        {shouldRenderQrReader && <QrReader constraints={{}} className="w-full my-auto" onResult={onResult} />}
+      <main className='flex items-center justify-center container max-w-lg mx-auto '>
+        <QrReader scanDelay={1000} constraints={{}} className="w-full" onResult={onResult} />
       </main>
     </div>
   )
