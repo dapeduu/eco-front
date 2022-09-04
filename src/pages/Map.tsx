b707cc214eb9ece592d04f80e280b08ce73f7fba
Pedro Santos <pedrosantosdevelop@gmail.com>
2022-09-04 22:55:28 +0000

    adds more info about the center

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -2,9 +2,11 @@ import { NavbarLayout } from "../components/NavbarLayout"
 import { Map as PigeonMap } from "pigeon-maps"
 
 export const Map = () => {
+  const center: [number, number] = [-15.79340, -47.88232] // Brasilia coords
+
   return (
     <NavbarLayout withBackButton>
-      <PigeonMap center={[-15.79340, -47.88232]}>
+      <PigeonMap center={center}>
       </PigeonMap>
     </NavbarLayout>
   )
